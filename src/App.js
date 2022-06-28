e86f4856417d0ed9cf8dec1bbe342a1b90aa6bbf
DanielPorojan <danielporojan280@gmail.com>
2022-06-28 17:09:59 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Die from './Die';
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
+
+
+export default function App(){
+  const [dice, setDice] = React.useState(allNewDice())
+    
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push({
+            value: Math.ceil(Math.random() * 6),
+            isHeld:false,
+            id:nanoid()
+          })
+      }
+      return newDice
+  }
+  
+  function rollDice(){
+    setDice(allNewDice())
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+        return die.id === id ? 
+            {...die, isHeld: !die.isHeld} :
+            die
+    }))
+}
+
+  const diceElements = dice.map(die => (
+    <Die 
+        key={die.id} 
+        value={die.value} 
+        isHeld={die.isHeld} 
+        holdDice={() => holdDice(die.id)}
+    />
+))
+
+  return(
+    <main className='main--content'>
+      <div className="grid">
+      {diceElements}
+      </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
+    </main>
+  )
+}
